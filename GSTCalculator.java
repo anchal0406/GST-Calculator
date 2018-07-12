646c69baabe0dc69fc20afd897d07d59a3da2fdc
anchal0406 <41166631+anchal0406@users.noreply.github.com>
2018-07-13 01:11:02 +0530

    Update GSTCalculator.java

--- GSTCalculator.java
+++ GSTCalculator.java
@@ -8,24 +8,24 @@ class GSTCalculator
     static final int electronics = 18;
     static final int cosmetics = 28;
     double gstFoodGrains, gstFurniture, gstElectronics, gstCosmetics;
-	double gstCalculatorFoodGrains(double initialPriceUnit)
+    double gstCalculatorFoodGrains(double initialPriceUnit)
     {
-		gstFoodGrains = initialPriceUnit * ((double)food_grains/(double)100);
+	gstFoodGrains = initialPriceUnit * ((double)food_grains/(double)100);
         return gstFoodGrains;
-	}
-	double gstCalculatorFurniture(double initialPriceUnit)
+    }
+    double gstCalculatorFurniture(double initialPriceUnit)
     {
-		gstFurniture = initialPriceUnit * ((double)furniture/(double)100);
+	gstFurniture = initialPriceUnit * ((double)furniture/(double)100);
         return gstFurniture;
-	}
-	double gstCalculatorElectronics(double initialPriceUnit)
+    }
+    double gstCalculatorElectronics(double initialPriceUnit)
     {
-		gstElectronics = initialPriceUnit * ((double)electronics/(double)100);
+	gstElectronics = initialPriceUnit * ((double)electronics/(double)100);
         return gstElectronics;
-	}
-	double gstCalculatorCosmetics(double initialPriceUnit)
+    }
+    double gstCalculatorCosmetics(double initialPriceUnit)
     {
-		gstCosmetics = initialPriceUnit * ((double)cosmetics/(double)100);
+	gstCosmetics = initialPriceUnit * ((double)cosmetics/(double)100);
         return gstCosmetics;
-	}
+    }
 }
