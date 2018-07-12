1f8328467b66e3656e787f209be25c2e80f57a22
anchal0406 <41166631+anchal0406@users.noreply.github.com>
2018-07-13 01:12:07 +0530

    Update FinalSellingPriceElectronics.java

--- FinalSellingPriceElectronics.java
+++ FinalSellingPriceElectronics.java
@@ -4,9 +4,9 @@ class FinalSellingPriceElectronics extends GSTCalculator
 	void finalSellingPriceElectronics(int unit,double initialPriceUnit)
     {
         //Stores electronics gst value of base class
-		double gst = super.gstElectronics;
-		System.out.println(gst);
-		double finalPriceElectronics = (initialPriceUnit+gst)*unit;
-		System.out.println(finalPriceElectronics);
-	}
+	double gst = super.gstElectronics;
+	System.out.println(gst);
+	double finalPriceElectronics = (initialPriceUnit+gst)*unit;
+	System.out.println(finalPriceElectronics);
+    }
 }
