14363f241abfd7ea1b027fd7d21fb1598e830a85
anchal0406 <41166631+anchal0406@users.noreply.github.com>
2018-07-13 01:12:40 +0530

    Update FinalSellingPriceFoodGrains.java

--- FinalSellingPriceFoodGrains.java
+++ FinalSellingPriceFoodGrains.java
@@ -4,9 +4,9 @@ class FinalSellingPriceFoodGrains extends GSTCalculator
 	void finalSellingPriceFoodGrains(int unit,double initialPriceUnit)
     {
         //Stores food grains gst value of base class 
-		double gst = super.gstFoodGrains; 
-		System.out.println(gst);
-		double finalPriceFoodGrains = (initialPriceUnit+gst)*unit;
-		System.out.println(finalPriceFoodGrains);
-	}
+	double gst = super.gstFoodGrains; 
+	System.out.println(gst);
+	double finalPriceFoodGrains = (initialPriceUnit+gst)*unit;
+	System.out.println(finalPriceFoodGrains);
+    }
 }
